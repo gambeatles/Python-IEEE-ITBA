3a19068d07869b27b1e33f839337917b8b8e9b98
gambeatles <gamontiel@hotmail.com.ar>
2022-08-15 11:24:23 -0300

    Desafio 1

---
+++ Desafio_1.py
@@ -0,0 +1,17 @@
+# Diseñar un programa en el cual se definan exactamente 2 variables.
+
+# La primera se debe llamar nombre y debe contener tu nombre (entre comillas).
+# La segunda se debe llamar edad y debe contener tu edad (como un número simple, sin comillas).
+# Por último el programa debe mostrar en pantalla la siguiente frase en una sóla línea:
+
+# (nombre) tiene (edad) años
+
+# Por ejemplo:
+
+# Mati tiene 5 años
+
+# Tip: La instrucción print() admite mostrar mensajes en un mismo renglón si se indican diferentes parámetros separados con comas.
+
+nombre = "Guillermo"
+edad = 43
+print(nombre,"tiene",edad,"años")
