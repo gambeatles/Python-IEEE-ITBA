25e7d92f791d815ebe682f08758ddf837807e1a4
gambeatles <gamontiel@hotmail.com.ar>
2022-08-15 11:33:54 -0300

    Ejemplo_2 Input

---
+++ Ejemplo_2.py
@@ -0,0 +1,11 @@
+# Input
+# Esto es un comentario!
+# Los comentarios no son ejecutados por Python
+# De esta forma el programador puede hacer una nota para humanos
+
+nombre = input("Ingrese su nombre: ")
+edad = int( input("Ingrese su edad: ") )
+
+print("nombre:", nombre)
+print("edad:", edad)
+
