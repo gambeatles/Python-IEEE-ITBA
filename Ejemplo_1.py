810ca5b8f6cbe635d27edc742263a88719567032
gambeatles <gamontiel@hotmail.com.ar>
2022-08-15 11:01:20 -0300

    Ejemplo 1 python

---
+++ Ejemplo_1.py
@@ -0,0 +1,34 @@
+# Output Variables
+
+print(123)
+print("Hola, cómo estas?")
+print('Buen día')
+
+print(1, 2, 3)
+print("Curso", "de", "Python")
+print('Uno:', 1, ' Dos:', 2, ' Tres:', 3)
+
+x = 5
+print(x)
+
+x = 'Hola'
+y = "Adios"
+z = 123
+print(x)
+print(y)
+print(z)
+
+mensaje = "Hola mundo!"
+print(mensaje)
+
+radio = 5
+pi = 3.14
+perimetro = 2 * pi * radio
+areaEsfera = 4 * pi * radio**2
+volumenEsfera = 4 / 3 * pi * radio**3
+
+print("radio =",radio)
+print('pi',pi)
+print("perimetro =",perimetro)
+print("areaEsfera =",areaEsfera)
+print("volumenEsfera",volumenEsfera)
