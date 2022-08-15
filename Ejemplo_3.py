065eb28a0bcdca8bee60903f3903c89749d25975
gambeatles <gamontiel@hotmail.com.ar>
2022-08-15 11:35:59 -0300

    Ejemplo_3 Operadores de modificacion

---
+++ Ejemplo_3.py
@@ -0,0 +1,17 @@
+# Operadores de modificación
+
+x = 3
+y = -2
+y = x*y
+x = x**2
+print("y =", y)
+print("x =", x)
+
+x = 17
+x += 3
+x /= 10
+print("x =",x)
+
+a = 12
+b = 5 + 7 / 4**5 * 3 - 7 * x + a
+print(b)
